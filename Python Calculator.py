80bd920188fcc68e55359b91cf7964b374dcdedf
kra3215 <158528136+kra3215@users.noreply.github.com>
2025-10-27 17:43:16 +0200

    Update Python Calculator.py

--- Python Calculator.py
+++ Python Calculator.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import StringVar
-
 def button_press(value): global equation_text; equation_text += value; equation_label.set(equation_text)
 def equals(): global equation_text; equation_text = str(eval(equation_text)) if equation_text else reset_equation("Error"); equation_label.set(equation_text)
 def clear(): reset_equation("")
@@ -15,3 +14,4 @@ for text, row, col in [("1", 0, 0), ("2", 0, 1), ("3", 0, 2), ("+", 0, 3), ("4",
     tk.Button(frame, text=text, height=2, width=5, font=("consolas", 15), command=(equals if text == "=" else lambda t=text: button_press(t))).grid(row=row, column=col, padx=5, pady=5)
 tk.Button(window, text="Clear", height=2, width=10, font=("consolas", 15), command=clear).pack(pady=10)
 window.mainloop()
+
